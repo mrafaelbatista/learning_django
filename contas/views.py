8bc0714a98ec8ea4f426aa721758cabbd24b9ff2
Messias Batista <mrafaelbatista@gmail.com>
2019-07-25 20:33:59 -0300

    Update

--- contas/views.py
+++ contas/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Transacao
+from .forms import TransacaoForm
 
 # My imports
 from django.http import HttpResponse
 import datetime
+
 
 # Create your views here.
 
@@ -12,4 +15,31 @@ def home(request):
     # html = "<html><body>It is now %s.</body></html>" % now
     # return HttpResponse(html)
 
-    return render(request, 'contas/home.html')
+    # exemplo de variáveis dentro do html
+    data = {
+        'now': datetime.datetime.now(),
+        'transacoes': ['t1', 't2', 't3']
+    }
+
+    return render(request, 'contas/home.html', data)
+
+
+def listagem(request):
+
+    data = {
+        'transacoes': Transacao.objects.all()
+    }
+
+    return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data = {
+        'form': form
+    }
+    return render(request, 'contas/form.html', data)
