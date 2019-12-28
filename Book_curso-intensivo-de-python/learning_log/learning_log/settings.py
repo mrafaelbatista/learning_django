23d022339d0adf546f860c742f42a4018e2f4af7
Messias R. Batista <mrafaelbatista@gmail.com>
2019-12-28 18:22:53 -0300

    Finished - Update Heroku
    This commit is the end project. The  last update is upload to Heroku and test the project. Its work.

--- Book_curso-intensivo-de-python/learning_log/learning_log/settings.py
+++ Book_curso-intensivo-de-python/learning_log/learning_log/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,5 @@ LOGIN_URL = '/login/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
